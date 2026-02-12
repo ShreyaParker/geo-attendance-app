ba0bad75839bfcf761386466a71b79de14a4c635
Shreya Parkar <shreyaparker317@gmail.com>
2026-02-12 17:23:00 +0530

    Add delete room route to roomRoutes

--- backend/routes/roomRoutes.js
+++ backend/routes/roomRoutes.js
@@ -7,4 +7,5 @@ router.post('/admin/create-room', roomController.createRoom);
 router.get('/admin/rooms', roomController.getAllRooms);
 router.post('/join-room', roomController.joinRoom);
 
+router.delete('/rooms/:id', deleteRoom); 
 export default router;
