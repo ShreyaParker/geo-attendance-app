c6f8da1a4eeceee47651882bf36c1327f782640f
Shreya Parkar <shreyaparker317@gmail.com>
2026-02-14 23:59:59 +0530

    Change password field to be required in User schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,25 +3,20 @@ import mongoose from 'mongoose';
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, default: '123456' },
+    password: { type: String, required: true },
     role: { type: String, enum: ['admin', 'employee'], default: 'employee' },
-
-
+    dateOfJoining: { type: Date, default: Date.now },
+    faceData: { type: String }, 
     joinedRoomCode: { type: String, default: null },
-
-
     isActiveShift: { type: Boolean, default: false },
     isInsideZone: { type: Boolean, default: false },
     lastLat: Number,
     lastLng: Number,
     lastSeen: Date,
-
-
     checkInImage: String,
     shiftStartTime: Date,
     lastExitTime: Date,
     currentShiftOutDuration: { type: Number, default: 0 },
-
     history: [{
         start: Date,
         end: Date,
