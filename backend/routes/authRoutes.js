d46d3041909a977d2cfadfa857b9751ac537f1e4
Shreya Parkar <shreyaparker317@gmail.com>
2026-02-15 19:38:25 +0530

    Add updateFaceModel route to authRoutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,8 @@
-import { login, register } from '../controllers/authController.js';
+import { login, register, updateFaceModel } from '../controllers/authController.js';
 import Router from "express"
 const router = Router();
 
 router.post('/login', login);
 router.post('/register', register);
-
+router.post('/update-face-model', verifyToken, updateFaceModel);
 export default router;
