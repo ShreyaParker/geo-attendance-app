2cb53a342364e47168a3797bb9664d1a18d0aee9
Shreya Parkar <shreyaparker317@gmail.com>
2026-02-19 21:41:49 +0530

    Add user details route to attendanceRoutes

--- backend/routes/attendanceRoutes.js
+++ backend/routes/attendanceRoutes.js
@@ -1,4 +1,10 @@
-import { updateStatus, getDashboard, getAlerts, getMonthlyAttendance } from '../controllers/attendanceController.js';
+import { 
+    updateStatus, 
+    getDashboard, 
+    getAlerts, 
+    getMonthlyAttendance,
+    getUserDetails 
+} from '../controllers/attendanceController.js';
 import express from "express";
 import {isAdmin, verifyToken} from "../middleware/auth.js";
 
@@ -8,6 +14,9 @@ router.post('/update-status', updateStatus);
 router.get('/admin/dashboard', getDashboard);
 router.get('/admin/dashboard/:roomCode', getDashboard);
 router.get('/admin/calendar', verifyToken, isAdmin, getMonthlyAttendance);
-router.get('/admin/alerts', verifyToken, isAdmin, getAlerts)
+router.get('/admin/alerts', verifyToken, isAdmin, getAlerts);
+
+
+router.get('/admin/user-details/:id', verifyToken, isAdmin, getUserDetails);
 
 export default router;
